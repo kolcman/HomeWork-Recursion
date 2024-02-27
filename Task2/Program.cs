808fc3f7325f07cba38ac6a565986ef4128d7c0d
Kolcman <m.kolcman@yandex.ru>
2024-02-27 15:16:22 +0300

    HomeWork:Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+void Main()
+{
+    string stringIsArrayOfChars = "dlrow olleH";
+    printReverseString(stringIsArrayOfChars, 0);
+}
+
+void printReverseString(string str, int index)
+{
+    if(index > str.Length -1) return;
+    printReverseString(str, index + 1);
+    System.Console.Write(str[index] + " ");
+}
+
+Main();
