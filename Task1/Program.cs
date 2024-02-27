a99a259e6634701814540c8f13867162748b9056
Kolcman <m.kolcman@yandex.ru>
2024-02-27 14:47:56 +0300

    HomeWork:Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿//  Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+void Main()
+{
+    int M = 5;
+    int N = 23;
+    printIntervalOfNumbers(M, N);
+
+}
+
+void printIntervalOfNumbers(int M, int N)
+{
+    if (M > N) return;
+    System.Console.Write(M + " ");
+    printIntervalOfNumbers(M + 1, N);
+}
+
+
+Main();
